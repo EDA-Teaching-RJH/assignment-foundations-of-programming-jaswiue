6d3256a57721f41f4caa89822466e5e524a818c5
Jasmine Lee <jl19@kent.ac.uk>
2026-02-04 16:20:59 +0000

    line 63 concanate string and interger issue

--- old_system.py
+++ old_system.py
@@ -59,7 +59,8 @@ def run_system_monolith():
             for rank in r:
                 if rank == "Captain" or rank =="Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+        #count is a string not a int, so replace + with ,          
+            print("High ranking officers: ", count) 
             
         elif opt == "5":
             print("Shutting down.")
