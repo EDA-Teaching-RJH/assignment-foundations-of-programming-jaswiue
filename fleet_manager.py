25012f26e8e90846a0478e99e06f1692b7539453
Jasmine Lee <jl19@kent.ac.uk>
2026-02-15 21:46:12 +0000

    funtion 2 the menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,3 +5,24 @@ def init_database():
     divs = ["command", "operations", "command", "operations", "sciences"]
     ids = ["1005", "1003", "1001", "1004", "1002"]
     return names, ranks, divs, ids,
+
+#function 2: display menu
+def display_menu():
+    full_name = input("What is your full name?")
+    print(f"\n Welcome, {full_name}!")
+    print("-_-_-_*- STARFLEET CREW MANAGEMENT -_-_*-_-")
+    print("1. Add Member")
+    print("2. Remove Member")
+    print("3. Update Rank")
+    print("4. display roster")
+    print("5. search crew")
+    print("6. filter by division")
+    print("7. calculate payroll")
+    print("8. count senior officers")
+    print("9. EXIT")
+
+    choice = input("select an option: ")
+    return choice
+    
+    
+    
