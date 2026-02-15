64766f78e1b8ee23479c4f1174879949b3701b49
Jasmine Lee <jl19@kent.ac.uk>
2026-02-15 21:28:12 +0000

    function 1 initial database

---
+++ fleet_manager.py
@@ -0,0 +1,7 @@
+#function 1: init_database
+def init_database():
+    names = ["beverly Crusher", "data", "jean-luc picard", "georgi la Forge", "william riker"]
+    ranks = ["commander", "lieutenant commander", "captain", "lieutenant", "commander"]
+    divs = ["command", "operations", "command", "operations", "sciences"]
+    ids = ["1005", "1003", "1001", "1004", "1002"]
+    return names, ranks, divs, ids,
